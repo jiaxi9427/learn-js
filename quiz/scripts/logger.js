a672b8e20b4d6349609dfeac0703a28b18feddb9
jiaxi9427 <Peng.jia@northeastern.edu>
2024-01-29 21:18:33 -0500

    update 0129

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,14 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(obj) {
+    if (obj && 'errMsg' in obj) {
+        console.error(obj.errMsg);
+    } else {
+        console.error("No error message available.");
+    }
+}
+
+const exampleObj1 = { errMsg: "Error: Invalid input." };
+const exampleObj2 = { errMsg: "Error: Unable to connect to server." };
+
+logMsg(exampleObj1);
+logMsg(exampleObj2);
